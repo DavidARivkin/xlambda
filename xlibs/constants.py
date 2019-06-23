bd26725c55e81e064c7017f4ef0a249c07a68e13
Renato Byrro <renato@dashbird.io>
2019-06-23 20:12:10 -0300

    Update constant values

--- xlibs/constants.py
+++ xlibs/constants.py
@@ -2,5 +2,15 @@
 import os
 
 
-STAGE = os.environ.get('STAGE')
-REGION = os.environ.get('REGION')
+# Basic settings
+DEFAULT_STAGE = 'dev'
+DEFAULT_REGION = 'us-east-1'
+STAGE = os.environ.get('STAGE', DEFAULT_STAGE)
+REGION = os.environ.get('REGION', DEFAULT_REGION)
+
+# Miscellaneous
+BASE_FUNCTION_NAME = 'xlambda-{function}-{stage}'
+
+# Forecasting
+CONFIDENCE_LEVEL = 0.9
+FORECAST_TIMEFRAME = 3  # How many periods of 5 minutes to forecast
--- xlibs/professor/constants.py
+++ xlibs/professor/constants.py
@@ -6,3 +6,21 @@ REQUIRED_ARGS = [
     's3_bucket',
     'config_obj_name'
 ]
+
+DEFAULT_CONFIG = {
+    'region': 'us-east-1',
+    'warm_payload': {
+        'xlambda': {
+            'warm': True,
+            'wait': {
+                'value': 10000,
+                'unit': 'milliseconds',
+            },
+        },
+    },
+    'scaling': {
+        'max_concurrency': 50,
+        'min_containers': 1,
+        'max_containers': 50,
+    },
+}
